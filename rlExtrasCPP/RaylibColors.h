834e05ec73d6944ab8a054cd89088db8297d1e69
Jeffery Myers <JeffM2501@gmail.com>
2021-05-19 09:23:04 -0700

    Add C++ colors header

---
+++ rlExtrasCPP/RaylibColors.h
@@ -0,0 +1,64 @@
+/**********************************************************************************************
+*
+*   raylibExtras * Utilities and Shared Components for Raylib
+*
+*   RLAssets * Simple Asset Managment System for Raylib
+*
+*   LICENSE: MIT
+*
+*   Copyright (c) 2020 Jeffery Myers
+*
+*   Permission is hereby granted, free of charge, to any person obtaining a copy
+*   of this software and associated documentation files (the "Software"), to deal
+*   in the Software without restriction, including without limitation the rights
+*   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*   copies of the Software, and to permit persons to whom the Software is
+*   furnished to do so, subject to the following conditions:
+*
+*   The above copyright notice and this permission notice shall be included in all
+*   copies or substantial portions of the Software.
+*
+*   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*   SOFTWARE.
+*
+**********************************************************************************************/
+
+#pragma once
+
+#include "raylib.h"
+
+namespace Colors
+{
+    constexpr ::Color  LightGray = { 200, 200, 200, 255 };
+    constexpr ::Color  DrarkGray{ 80, 80, 80, 255 };
+    constexpr ::Color  Yellow{ 253, 249, 0, 255 };
+    constexpr ::Color  Gold{ 255, 203, 0, 255 };
+    constexpr ::Color  Orange{ 255, 161, 0, 255 };
+    constexpr ::Color  Pink{ 255, 109, 194, 255 };
+    constexpr ::Color  Red{ 230, 41, 55, 255 };
+    constexpr ::Color  Maroon{ 190, 33, 55, 255 };
+    constexpr ::Color  Green{ 0, 228, 48, 255 };
+    constexpr ::Color  Lime{ 0, 158, 47, 255 };
+    constexpr ::Color  DarkGreen{ 0, 117, 44, 255 };
+    constexpr ::Color  SkyBlue{ 102, 191, 255, 255 };
+    constexpr ::Color  Blue{ 0, 121, 241, 255 };
+    constexpr ::Color  DarkBlue{ 0, 82, 172, 255 };
+    constexpr ::Color  Purple{ 200, 122, 255, 255 };
+    constexpr ::Color  Violet{ 135, 60, 190, 255 };
+    constexpr ::Color  DarkPurpple{ 112, 31, 126, 255 };
+    constexpr ::Color  Beige{ 211, 176, 131, 255 };
+    constexpr ::Color  Brown{ 127, 106, 79, 255 };
+    constexpr ::Color  DarkBrown{ 76, 63, 47, 255 };
+
+    constexpr ::Color  White{ 255, 255, 255, 255 };
+    constexpr ::Color  Black{ 0, 0, 0, 255 };
+    constexpr ::Color  Blank{ 0, 0, 0, 0 };
+    constexpr ::Color  Magenta{ 255, 0, 255, 255 };
+    constexpr ::Color  RayWhite{ 245, 245, 245, 255 };
+
+}
