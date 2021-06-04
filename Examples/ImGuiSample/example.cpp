fdb5839187c80f577bd2d010a974eeafc3fa67c3
Jeffery Myers <JeffM2501@gmail.com>
2021-06-04 16:24:04 -0700

    Update sample to include more stuff

--- Examples/ImGuiSample/example.cpp
+++ Examples/ImGuiSample/example.cpp
@@ -20,9 +20,137 @@
 ********************************************************************************************/
 
 #include "raylib.h"
+#include "raymath.h"
 
 #include "imgui.h"
 #include "rlImGui.h"
+
+bool Quit = false;
+
+bool ImGuiDemoOpen = false;
+bool ImageEditorWindowOpen = true;
+
+RenderTexture ImageScratchPad;
+Texture ImageTexture;
+Camera2D ScratchCamera = { 0 };
+
+Vector2 LastMousePos = { 0 };
+Vector2 LastTarget = { 0 };
+bool Dragging = false;
+
+enum class ToolMode
+{
+	None,
+	Move,
+	Zoom
+};
+
+ToolMode CurrentToolMode = ToolMode::None;
+
+void DoMainMenu()
+{
+	if (ImGui::BeginMainMenuBar())
+	{
+		if (ImGui::BeginMenu("File"))
+		{
+			if (ImGui::MenuItem("Exit"))
+				Quit = true;
+
+			ImGui::EndMenu();
+		}
+
+		if (ImGui::BeginMenu("Window"))
+		{
+			ImGui::MenuItem("ImGui Demo", nullptr, &ImGuiDemoOpen);
+			ImGui::MenuItem("Image Editor", nullptr, &ImageEditorWindowOpen);
+
+			ImGui::EndMenu();
+		}
+		ImGui::EndMainMenuBar();
+	}
+}
+
+void ShowImageEditor()
+{
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
+	ImGui::SetNextWindowSizeConstraints(ImVec2(400, 400), ImVec2((float)GetScreenWidth(), (float)GetScreenHeight()));
+
+	if (ImGui::Begin("Image Viewer", &ImageEditorWindowOpen,ImGuiWindowFlags_NoScrollbar))
+	{
+		if (ImGui::BeginChild("Toolbar",ImVec2(ImGui::GetContentRegionAvailWidth(),25)))
+		{
+			ImGui::SetCursorPosX(2);
+			ImGui::SetCursorPosY(3);
+
+            if (ImGui::Button("None"))
+            {
+				CurrentToolMode = ToolMode::None;
+            }
+            ImGui::SameLine();
+
+			if (ImGui::Button("Move"))
+			{
+                CurrentToolMode = ToolMode::Move;
+			}
+			ImGui::SameLine();
+
+			if (ImGui::Button("Zoom"))
+			{
+                CurrentToolMode = ToolMode::Zoom;
+			}
+
+			ImGui::SameLine();
+			switch (CurrentToolMode)
+			{
+			case ToolMode::None:
+				ImGui::TextUnformatted("No Tool");
+				break;
+			case ToolMode::Move:
+				ImGui::TextUnformatted("Move Tool");
+				break;
+			case ToolMode::Zoom:
+				ImGui::TextUnformatted("Zoom Tool");
+				break;
+			default:
+				break;
+			}
+
+			ImGui::SameLine();
+			ImGui::TextUnformatted(TextFormat("camera target X%f Y%f", ScratchCamera.target.x, ScratchCamera.target.y));
+			ImGui::EndChild();
+		}
+
+
+		ImVec2 size = ImGui::GetContentRegionAvail();
+
+		// center the scratchpad in the view
+		Rectangle viewRect = { 0 };
+		viewRect.x = ImageScratchPad.texture.width / 2 - size.x / 2;
+		viewRect.y = ImageScratchPad.texture.height / 2 - size.y / 2;
+		viewRect.width = size.x;
+		viewRect.height = size.y;
+
+		// draw the scratchpad
+		RLImGuiImageRect(&ImageScratchPad.texture, (int)size.x, (int)size.y, viewRect);
+
+		
+		ImGui::SetCursorPos(ImVec2(0, 0));
+		// some tools
+
+		ImGui::End();
+	}
+	ImGui::PopStyleVar();
+}
+
+void UpdateRenderTexture()
+{
+	BeginTextureMode(ImageScratchPad);
+	ClearBackground(BLUE);
+	BeginMode2D(ScratchCamera);
+	DrawTexture(ImageTexture, ImageTexture.width/-2, ImageTexture.height/-2,WHITE);
+	EndMode2D();
+	EndTextureMode();
+}
 
 int main(int argc, char* argv[])
 {
@@ -35,15 +163,89 @@ int main(int argc, char* argv[])
 	InitWindow(screenWidth, screenHeight, "raylib [ImGui] example - ImGui Demo");
 	//SetTargetFPS(120);
 	SetupRLImGui(true);
+	ImGui::GetIO().ConfigWindowsMoveFromTitleBarOnly = true;
+
+	ImageScratchPad = LoadRenderTexture(GetScreenWidth(), GetScreenHeight());
+	Image testImage = GenImageCellular(800, 800, 25);
+	ImageTexture = LoadTextureFromImage(testImage);
+	UnloadImage(testImage);
+
+	ScratchCamera.zoom = 1;
+	ScratchCamera.target.x = 0;
+	ScratchCamera.target.y = 0;
+	ScratchCamera.rotation = 0;
+	ScratchCamera.offset.x = GetScreenWidth() / 2.0f;
+	ScratchCamera.offset.y = GetScreenHeight() / 2.0f;
+
+	UpdateRenderTexture();
+
+	LastMousePos = GetMousePosition();
 
 	// Main game loop
-	while (!WindowShouldClose())    // Detect window close button or ESC key
+	while (!WindowShouldClose() && !Quit)    // Detect window close button or ESC key
 	{
+		if (IsWindowResized())
+		{
+			UnloadRenderTexture(ImageScratchPad);
+			ImageScratchPad = LoadRenderTexture(GetScreenWidth(), GetScreenHeight());
+
+            ScratchCamera.offset.x = GetScreenWidth() / 2.0f;
+            ScratchCamera.offset.y = GetScreenHeight() / 2.0f;
+		}
+
+		if (CurrentToolMode == ToolMode::Move)
+		{
+			if (IsMouseButtonDown(0))
+			{
+				if (!Dragging)
+				{
+                    LastMousePos = GetMousePosition();
+                    LastTarget = ScratchCamera.target;
+				}
+				Dragging = true;
+                Vector2 mousePos = GetMousePosition();
+                Vector2 mouseDelta = Vector2Subtract(LastMousePos, mousePos);
+
+				mouseDelta.x /= ScratchCamera.zoom;
+				mouseDelta.y /= ScratchCamera.zoom;
+                ScratchCamera.target = Vector2Add(LastTarget, mouseDelta);
+
+				UpdateRenderTexture();
+			}
+			else
+			{
+				Dragging = false;
+			}
+		}
+		else if (CurrentToolMode == ToolMode::Zoom)
+		{
+			if (IsMouseButtonPressed(0))
+			{
+				Vector2 mousePos = GetMousePosition();
+				Vector2 localCameraPos = GetScreenToWorld2D(mousePos, ScratchCamera);
+				ScratchCamera.target = localCameraPos;
+
+				if (IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_SHIFT))
+					ScratchCamera.zoom /= 1.25f;
+				else
+					ScratchCamera.zoom *= 1.25f;
+
+                UpdateRenderTexture();
+			}
+		}
+
 		BeginDrawing();
 		ClearBackground(DARKGRAY);
 
 		BeginRLImGui();
-		ImGui::ShowDemoWindow(NULL);
+		DoMainMenu();
+
+		if (ImGuiDemoOpen)
+			ImGui::ShowDemoWindow(&ImGuiDemoOpen);
+
+		if (ImageEditorWindowOpen)
+			ShowImageEditor();
+
 		EndRLImGui();
 
 		EndDrawing();
