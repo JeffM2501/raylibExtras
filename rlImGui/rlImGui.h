d21d95681dad7b929a21c15b4d847e071e32bdd2
Jeffery Myers <jeffm2501@gmail.com>
2021-06-05 18:17:38 -0700

    ImGui color converter

--- rlImGui/rlImGui.h
+++ rlImGui/rlImGui.h
@@ -29,7 +29,17 @@
 **********************************************************************************************/
 
 #pragma once
+
 #include "raylib.h"
+#include "imgui.h"
+
+namespace ImGuiColors
+{
+    inline ImVec4 Convert(::Color&& color)
+    {
+        return ImVec4(color.r / 255.0f, color.g / 255.0f, color.b / 255.0f, color.a / 255.0f);
+    }
+}
 
 void SetupRLImGui(bool dark);
 void BeginRLImGui();
