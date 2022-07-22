6c505d514123f81a1235a0e5a53f69495fa6a19a
Julianiolo <50519317+Julianiolo@users.noreply.github.com>
2022-07-22 21:07:10 +0200

    rlImGui fix Crash on empty RenderTriangle Calls

--- rlImGui/rlImGui.cpp
+++ rlImGui/rlImGui.cpp
@@ -274,6 +274,9 @@ static void rlImGuiTriangleVert(const ImDrawVert& idx_vert)
 
 static void rlImGuiRenderTriangles(unsigned int count, int indexStart, const ImVector<ImDrawIdx>& indexBuffer, const ImVector<ImDrawVert>& vertBuffer, void* texturePtr)
 {
+    if (count == 0)
+        return;
+
     Texture* texture = (Texture*)texturePtr;
 
     unsigned int textureId = (texture == nullptr) ? 0 : texture->id;
